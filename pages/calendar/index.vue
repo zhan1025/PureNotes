bbb051053b55f005e69580b7e1329b28bbfae1e9
zhan1025 <argus.zhan@genew.com>
2020-04-11 18:19:06 +0800

    添加日历组件(待优化)、tabBar

---
+++ pages/calendar/index.vue
@@ -0,0 +1,24 @@
+<template>
+	<view>
+		<calendar @onCancel="onCancel" @onConfirm="onConfirm" :startDate="startDate" :endDate="endDate" :defaultValue="pickerDate">
+		    {{pickerDate}}
+		</calendar>
+	</view>
+</template>
+
+<script>
+	import calendar from '@/components/calendar/calendar.vue'
+	export default {
+		data(){
+			return {
+				pickerDate:new Date()
+			}
+		},
+		components:{
+		        calendar
+		    }
+	}
+</script>
+
+<style>
+</style>
